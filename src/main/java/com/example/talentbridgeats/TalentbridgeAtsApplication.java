d37ae97c1d73e98abd7d9a0bc9dd57b58d3124ab
Kasundi Ranaweera <kasundiranaweera@gmail.com>
2026-08-05 13:56:18 +0530

    Project scaffold: Spring Boot 4, MySQL, JWT deps, DB connectivity

---
+++ src/main/java/com/example/talentbridgeats/TalentbridgeAtsApplication.java
@@ -0,0 +1,13 @@
+package com.example.talentbridgeats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentbridgeAtsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalentbridgeAtsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/talentbridgeats/TalentbridgeAtsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.talentbridgeats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentbridgeAtsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
